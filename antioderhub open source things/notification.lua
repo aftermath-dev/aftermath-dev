06bc6ae8529cbd0b9600377d8ea64a5b8479c953
Aftr <evgenijzulin629@gmail.com>
2025-02-24 22:35:12 +0700

    Create notification.lua

---
+++ antioderhub open source things/notification.lua
@@ -0,0 +1,8 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "AntiOder hub is loading...";
+    Text = "Script can load 5-22 seconds";
+    Duration = 5;  -- Durée de la notification en secondes
+    Button1 = "OK"; -- Bouton optionnel
+})
