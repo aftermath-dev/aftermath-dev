6f8c29ca896452a583a7a9df4b2c9f88d34073b3
Aftr <evgenijzulin629@gmail.com>
2025-06-04 22:37:57 +0700

    Update toggle.lua

--- toggle.lua
+++ toggle.lua
@@ -2,6 +2,14 @@ local togglegui = Instance.new("ScreenGui")
 local TextButton = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
 local UIGradient = Instance.new("UIGradient")
+local UserInputService = game:GetService("UserInputService")
+local isMobile = false
+
+if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
+ isMobile = true
+elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
+ print("PC detected! Hiding toggle...")
+end
 
 --Properties:
 
@@ -35,7 +43,6 @@ TextButton.MouseButton1Click:Connect(function()
     keypress(Enum.KeyCode.LeftControl)
 end)
 
-local UserInputService = game:GetService("UserInputService")
 
 local gui = TextButton
 
@@ -45,32 +52,32 @@ local dragStart
 local startPos
 
 local function update(input)
-	local delta = input.Position - dragStart
-	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+  local delta = input.Position - dragStart
+  gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 end
 
 gui.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-		dragging = true
-		dragStart = input.Position
-		startPos = gui.Position
+  if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+    dragging = true
+    dragStart = input.Position
+    startPos = gui.Position
 
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragging = false
-			end
-		end)
-	end
+    input.Changed:Connect(function()
+      if input.UserInputState == Enum.UserInputState.End then
+        dragging = false
+      end
+    end)
+  end
 end)
 
 gui.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		dragInput = input
-	end
+  if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+    dragInput = input
+  end
 end)
 
 UserInputService.InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		update(input)
-	end
+  if input == dragInput and dragging then
+    update(input)
+  end
 end)
